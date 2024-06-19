7672e4e524f8a1719cb56f88b8e9bf498c6a63bb
palnex <thepalnex010@gmail.com>
2024-06-18 20:54:41 -0700

    Initial commit with new code

---
+++ Scenes/Player.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public partial class Player : StaticBody2D
+{ 
+	int win_height;
+	int p_height;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		win_height = (int)GetViewportRect().Size.Y;
+		
+		//GetNode<Godot.ColorRect>("ColorRect").Size.y;
+
+		p_height = (int)GetNode<Godot.ColorRect>("ColorRect").Size.Y;
+	
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+
+		if (Input.IsActionPressed("ui_up")){
+			Position = new Vector2(Position.X, Position.Y- main.PADDLE_SPEED* (float)delta);
+			//Position -+ new Vector2(0, Main.PADDLE_SPEED * (float)delta);
+		}
+
+		if (Input.IsActionPressed("ui_down")){
+			Position = new Vector2(Position.X, Position.Y+ main.PADDLE_SPEED* (float)delta);
+		}
+		Position = new Vector2(Position.X, Mathf.Clamp(Position.Y, p_height / 2, win_height - p_height / 2));
+
+	}
+}
