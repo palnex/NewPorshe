3acdb1e203b457e81c3468319890a2cffa54c656
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2024-06-21 15:57:49 -0700

    bug-fixes

--- NewPorshh/Scenes/Ball.cs
+++ NewPorshh/Scenes/Ball.cs
@@ -7,12 +7,33 @@ public partial class Ball : CharacterBody2D
 	const int START_SPEED = 500;
 	const int ACCEL = 50;
 	int speed;
-
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	//public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	
+	//A custom signal
+	[Signal]
+	public delegate void OnScoreLeftEventHandler();
+	
+	//A custom signal
+	[Signal]
+	public delegate void OnScoreRightEventHandler();
 
 	public override void _PhysicsProcess(double delta)
 	{
-	
+		
+	}
+	public override void _Ready()
+	{
+		
+	}
+
+	//Listens to the BodyEntered Signal provided by Godot.
+	private void OnScoreLeftBodyEntered(Ball ball){
+		//Starts up a custom signal for subscribers to listen (aka. an event)
+		EmitSignal(SignalName.OnScoreLeft);
+	}
+
+	//Listens to the BodyEntered Signal provided by Godot.
+	private void OnScoreRightBodyEntered(Ball ball){
+		//Starts up a custom signal for subscribers to listen (aka. an event)
+		EmitSignal(SignalName.OnScoreLeft);
 	}
 }
